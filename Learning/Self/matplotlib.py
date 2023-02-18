c414fbc64b7a5ec848c7eb2c73f987ccddb4be36
haduc25 <haducvcvb@gmail.com>
2023-02-18 20:37:10 +0700

    handle Matplotlib / 18.02.2023

---
+++ Learning/Self/matplotlib.py
@@ -0,0 +1,20 @@
+#Matplotlib
+
+import numpy as np
+# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+# # import matplotlib.pyplot as plt   
+   
+# #required: pip install matplotlib
+
+
+# Tính tọa độ x, y cho đồ thị hình sin
+# x = np.arange(0, 3 * np.pi, 0.1)
+# y = np.sin(x)
+
+# # Plot the points using matplotlib
+# plt.plot(x, y)
+# plt.show() # You must call plt.show() to make graphics appear.
+
+print('#'*5)
+print('#'*5)
