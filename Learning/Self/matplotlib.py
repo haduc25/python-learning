84baf95d78fa8cb656d88948c627d6db12e965a4
haduc25 <haducvcvb@gmail.com>
2023-02-19 13:30:31 +0700

    testing some math #1 /19.02.2023

--- Learning/Self/matplotlib.py
+++ Learning/Self/matplotlib.py
@@ -1,20 +1,43 @@
-#Matplotlib
+# #Matplotlib
 
-import numpy as np
-# import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
-# # import matplotlib.pyplot as plt   
+# import numpy as np
+# import matplotlib.pyplot as plt   #lỗi chua fix đc
+
+# # import sys
+# # print(sys.path) 
    
-# #required: pip install matplotlib
+# # #required: pip install matplotlib
+
+# print('#'*5)
+# print('#'*5)
 
 
-# Tính tọa độ x, y cho đồ thị hình sin
-# x = np.arange(0, 3 * np.pi, 0.1)
-# y = np.sin(x)
+'''
+https://colab.research.google.com/drive/1cQKsbnzxolUKcNdIi9_Bk_cLDC2byoGL?authuser=1
+@author: zhaosong
+'''
+# Import pyplot module and alias it as plt. This can avoid repeatly call pyplot.
+import matplotlib.pyplot as plt
 
-# # Plot the points using matplotlib
-# plt.plot(x, y)
-# plt.show() # You must call plt.show() to make graphics appear.
+# Draw a point based on the x, y axis value.
+def draw_point():
+    # Draw a point at the location (3, 9) with size 1000
+    plt.scatter(3, 9, s=1000)
 
-print('#'*5)
-print('#'*5)
+    # Set chart title.
+    plt.title("Square Numbers", fontsize=19)
+
+    # Set x axis label.
+    plt.xlabel("Number", fontsize=10)
+
+    # Set y axis label.
+    plt.ylabel("Square of Number", fontsize=10)
+
+    # Set size of tick labels.
+    plt.tick_params(axis='both', which='major', labelsize=9)
+
+    # Display the plot in the matplotlib's viewer.
+    plt.show()
+
+if __name__ == '__main__':
+    draw_point()
