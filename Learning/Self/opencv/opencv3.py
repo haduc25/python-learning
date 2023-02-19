8c1bb6773a31d74a4230498dafa42d21b07ab7bf
haduc25 <haducvcvb@gmail.com>
2023-02-19 17:23:18 +0700

    opencv - set width, height & chia 4 frame #8 / 19.02.2023

--- Learning/Self/opencv/opencv3.py
+++ Learning/Self/opencv/opencv3.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2 as cv
 capture = cv.VideoCapture(0)  # Nếu có nhiều webcam thì id 1, 2, 3...
 
@@ -13,8 +14,45 @@ while True:
     ret, frame = capture.read()  # đọc img & chuyển về ret, frame
     print(ret)
 
+    # set width, height
+    width = int(capture.get(3))
+    height = int(capture.get(4))
+
+    # resize / small = 50% frame ban đầu
+    small_frame = cv.resize(frame, (0, 0), fx=.5, fy=.5)
+
+    # create a image only black with value is 0, size = webcam
+    image = np.zeros(frame.shape, np.uint8)
+
+    """_summary_
+        image = np.zeros(shape, dtype)
+        https://numpy.org/doc/stable/reference/generated/numpy.zeros.html
+        
+        # data type: np.uint8
+        
+        ###################################
+        width = int(capture.get(3)) 
+            - Ép kiểu
+            - capture.get(3) => 3 là chiểu rộng
+            - capture.get(4) => 4 là chiểu cao
+    """
+
+    # chia ra 4 frame | `//` chia hết số nguyên
+    # image 1 - từ đầu đến giữa chiều cao chia 2, từ đầu đến giữa chiều ngang chia 2 | top left
+    image[:height//2, :width//2] = small_frame
+
+    # image 2 - từ đầu đến giữa chiều cao chia 2, từ giữa chiều ngang chia 2 đến cuối | top right
+    image[:height//2, width//2:] = small_frame
+
+    # image 3 - từ chiều cao chia 2 đến cuối, từ đầu đến chiều ngang chia 2 | bottom left
+    image[height//2:, :width//2] = small_frame
+
+    # image 4 - từ chiều cao chai 2 đến cuối, từ chiều ngang chia 2 đến cuối | bottom right
+    image[height//2:, width//2:] = small_frame
+
     # show img
-    cv.imshow('My Capture', frame)
+    cv.imshow('My Capture', image)
+    # cv.imshow('My Capture', frame)
 
     # waiting key / 1s
     if (cv.waitKey(1) == ord('q')):
