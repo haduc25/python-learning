e4155f09727bfa452b248698fd5e8fdd073fb3cc
haduc25 <haducvcvb@gmail.com>
2023-02-19 16:03:56 +0700

    open cv - overide a image #5 / 19.02.2023

--- Learning/Self/opencv/opencv2.py
+++ Learning/Self/opencv/opencv2.py
@@ -1,11 +1,12 @@
 import cv2 as cv
+import random
 
 # read img
 img = cv.imread('Learning/Self/opencv/img/love.jpg', 1)
 # img = cv.imread('Learning/Self/opencv/img/2.PNG', -1)
 
 # resize img
-# img = cv.resize(img, (0, 0), fx=.5, fy=.5)
+img = cv.resize(img, (0, 0), fx=.5, fy=.5)
 
 
 # array of image
@@ -20,7 +21,7 @@ print(img.shape)  # (741, 412, 3), Chiều cao => hàng & chiều rộng => cộ
 """_summary_
     #########################
     print(img.shape) 
-    # (741, 412, 3), Chiều cao => hàng & chiều rộng => cột
+    # (741, 412, 3), Chiều cao => hàng & chiều rộng => cột | (row, col, channel)
     741 - Chiều cao => hàng
     412 - chiều rộng => cột
     3 - kênh (chanel) => RGB, RGBA
@@ -34,12 +35,27 @@ print(img.shape)  # (741, 412, 3), Chiều cao => hàng & chiều rộng => cộ
 # xuất dữ liệu của img theo index
 print('#'*32)
 # (1482, 824, 3)
-print(img[0])  # value of index 0
-print(img[1481])  # value of index 1481, because index from 0 - 1481
+# print(img[0])  # value of index 0
+# print(img[1481])  # value of index 1481, because index from 0 - 1481
 
 
+# overide a image
+for i in range(100):
+    # img.shape[1]: 1482 (row), img.shape[1]: 824 (col)
+    for j in range(img.shape[1]):
+        # pass
+        # gán random color cho từng pixel
+        # img[i][j] = [255, 255, 255]
+        img[i][j] = [random.randint(0, 255), random.randint(
+            0, 255), random.randint(0, 255)]  # [G, B, G]
+
+    """_summary_
+        In Python, the `pass` keyword is a placeholder statement that does nothing. It is often used as a placeholder in situations where the interpreter expects a statement, but you don't want to execute any code.
+        def my_function():
+            pass  # placeholder until we add code
+    """
 # show img
-# cv.imshow('My Crush', img)
+cv.imshow('My Crush', img)
 
 # stop window
-# cv.waitKey()
+cv.waitKey()
