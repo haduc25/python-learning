302368f2b1e52ee0dd54dcd15e07211551150e88
lucifer01925 <ducbk2k1@gmail.com>
2023-02-28 23:42:01 +0700

    python - tensorflow & keras #1 / 28.01.2023

---
+++ Learning/Self/TensorFlow & Keras/chatGPT/checking_tf_keras.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import keras
+
+print("TensorFlow version:", tf.__version__)  # TensorFlow version: 2.11.0
+print("Keras version:", keras.__version__)  # Keras version: 2.11.0
