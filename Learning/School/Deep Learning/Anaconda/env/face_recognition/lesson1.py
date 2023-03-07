d67bb4e1c970df233d69bf4ebd39c143adbfbe1a
lucifer01925 <ducbk2k1@gmail.com>
2023-03-07 17:16:46 +0700

    Face_recognition - putText, calc sai so, do tin cay #4 / 07.03.2023

--- Learning/School/Deep Learning/Anaconda/env/face_recognition/lesson1.py
+++ Learning/School/Deep Learning/Anaconda/env/face_recognition/lesson1.py
@@ -17,8 +17,15 @@ imgMaiLan = cv2.cvtColor(imgMaiLan, cv2.COLOR_BGR2RGB)
 
 
 # image cần nhận diện
+# unknownImg = face_recognition.load_image_file(
+#     'img/classify/do_mai_lan/dml2.jpg')
+
+# example other image 
 unknownImg = face_recognition.load_image_file(
-    'img/classify/do_mai_lan/dml2.jpg')
+    'img/classify/nguyen_tu_anh/nta1.jpg')
+
+# unknownImg = face_recognition.load_image_file(
+#     'img/classify/nguyen_tu_anh/nta2.jpg')
 
 # Chuyển BGR => RGB
 imgMaiLan = cv2.cvtColor(imgMaiLan, cv2.COLOR_BGR2RGB)
@@ -75,13 +82,55 @@ cv2.rectangle(unknownImg, (unknownFaceLocation[3], unknownFaceLocation[0]), (unk
 # =============================================================================
 
 # Calculate the desired width for each image
-width = min(int(imgMaiLan.shape[1]*0.5), int(unknownImg.shape[1]*0.5))
+# width = min(int(imgMaiLan.shape[1]*0.5), int(unknownImg.shape[1]*0.5)) # 50%
+width = min(int(imgMaiLan.shape[1]*0.8), int(unknownImg.shape[1]*0.8)) # 80%
 
 # Resize the images to the desired width
 imgMaiLan = cv2.resize(imgMaiLan, (width, int(imgMaiLan.shape[0]*width/imgMaiLan.shape[1])))
 unknownImg = cv2.resize(unknownImg, (width, int(unknownImg.shape[0]*width/unknownImg.shape[1])))
 
 
+# =============================================================================
+# So sánh image/ compare => face_recognition.compare_faces(known_face_encodings, face_encoding_to_check)
+# =============================================================================
+
+imgResult = face_recognition.compare_faces([encodeMailan], encodeUnknowImg)
+
+print('imgResult: ', imgResult) # True / False
+
+# =============================================================================
+# So sánh khi có nhiều ảnh: khoảng cách (sai số) giữa các bức ảnh? 
+# => face_recognition.face_distance(face_encodings, face_to_compare) | distance
+# faceDistance còn thấp thì càng giống
+# =============================================================================
+
+faceDistance = face_recognition.face_distance([encodeMailan], encodeUnknowImg)
+print('faceDistance (sai số): ', faceDistance, type(faceDistance)) # ndarray
+print(f'faceDistance (sai số - quy đổi theo thang 100%): {faceDistance * 100}%')
+
+
+# =============================================================================
+# add faceDistance to image => unknown image 
+# =============================================================================
+
+# vì faceDistance là array => cần index | faceDistance[0] 
+# dùng round() làm tròn 2 số => round(faceDistance[0], 2)
+resultTextOfImage = f'{imgResult} - {round(faceDistance[0], 2)*100}%' # ra sai số
+
+# chuyển từ sai số qua tỷ lệ % giống nhau (1 - sai số) | => độ tin cậy | Confidence interval
+resultTextOfImageConfidenceInterval = f'{imgResult} - {round((1 - faceDistance[0]) * 100, 2)}%' # ra dộ tin cậy
+
+
+
+print('resultTextOfImage: ', resultTextOfImage)
+print('resultTextOfImageConfidenceInterval: ', resultTextOfImageConfidenceInterval)
+
+# add text | sai số
+# cv2.putText(unknownImg, resultTextOfImage, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 255, 0), 2)
+
+# add text | độ tin cậy
+cv2.putText(unknownImg, resultTextOfImageConfidenceInterval, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 255, 0), 2)
+    
 
 # =============================================================================
 # Show image
