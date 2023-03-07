e6a71500a4c62af007fa112f3fb853bf4c3bd61d
lucifer01925 <ducbk2k1@gmail.com>
2023-03-07 19:06:03 +0700

    Face_recognition - load image, os.path.splitext() #6 / 07.03.2023

--- Learning/School/Deep Learning/Anaconda/env/face_recognition/lesson2.py
+++ Learning/School/Deep Learning/Anaconda/env/face_recognition/lesson2.py
@@ -5,3 +5,78 @@ Created on Tue Mar  7 17:40:38 2023
 @author: Admin
 """
 
+import cv2
+import face_recognition
+import os
+
+# =============================================================================
+# STEP 1: LOAD IMAGE FROM FOLDER (IMAGE FOR TRAINING) 
+# =============================================================================
+
+# path
+pathFolder = 'img/library'
+
+# create empty array
+images = []
+classNames = []
+
+# create a list => load image from folder to list
+myList = os.listdir(pathFolder)
+print('myList: ', myList) # ['Ban_Mai_Huyen_1.jpg', 'Do_Mai_Lan_1.jpg', 'Do_Mai_Lan_2.jpg', 'Nguyen_Tu_Anh_1.jpg', 'Nguyen_Tu_Anh_2.jpg']
+
+
+# loop get image 1 by 1
+for imgName in myList:
+    print('image: ', imgName)
+    """
+    image:  Ban_Mai_Huyen_1.jpg
+    image:  Do_Mai_Lan_1.jpg
+    image:  Do_Mai_Lan_2.jpg
+    image:  Nguyen_Tu_Anh_1.jpg
+    image:  Nguyen_Tu_Anh_2.jpg
+    """
+    
+    # current iamge - ảnh hiện tại | đọc từng ảnh 1 => đẩy ma trận điểm ảnh (image) vào array 'images'
+    curImage = cv2.imread(f'{pathFolder}/{imgName}') #img/library/imgName == 'Ban_Mai_Huyen_1.jpg', 'Do_Mai_Lan_1.jpg', 'Do_Mai_Lan_2.jpg', 'Nguyen_Tu_Anh_1.jpg', 'Nguyen_Tu_Anh_2.jpg'
+    
+    # add 'curImage' to array 'images' | arr.append()
+    images.append(curImage)
+
+    """ os.path.splitext(imgName)[0] | splittext(): tách path ra thành 2 phần: 1. trước phần đuôi mở rộng (fileName), 2. phần đuôi mở rộng (ext) 
+        # tách tên file vs đuôi file ra (test.txt => ['test', '.txt]) => exam-splitText.py
+        # tách ra & append() to array 'classNames'
+        # os.path.splitext(imgName)[0]: cắt ra lấy phần tên | Ban_Mai_Huyen_1.jpg => lấy phần 'Ban_Mai_Huyen_1' & bỏ '.jpg' => '.jpg' ở index 1 
+        # => lấy image name => bỏ ext (đuôi file) => add cái image name và trong array 'classNames' => arr.append()
+    """
+    classNames.append(os.path.splitext(imgName)[0])
+    
+
+# print length of array
+print('\nlength of array images is', len(images)) # 5   
+print('className is', classNames) # ['Ban_Mai_Huyen_1', 'Do_Mai_Lan_1', 'Do_Mai_Lan_2', 'Nguyen_Tu_Anh_1', 'Nguyen_Tu_Anh_2']
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
