bf077e886285c428a5d8ca10cd2cdb70dbcc50e3
haduc25 <haducvcvb@gmail.com>
2023-04-04 23:17:12 +0700

    added docs

---
+++ Learning/Self/lesson2.py
@@ -0,0 +1,12 @@
+# Lambda function
+
+# Add 10 to argument a, and return the result:
+def x(a): return a + 10
+
+
+print(x(5))  # 15
+
+# Multiply argument a with argument b and return the result:
+
+
+def multiplyArgument
